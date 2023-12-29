9df7b56182082030c817782b15462aebda22fc02
hannahfantasia <REDACTED>
2023-12-29 15:54:34 +0100

    refactoring fetches n critters

--- critterworld/critterworld.js
+++ critterworld/critterworld.js
@@ -1,53 +1,55 @@
-const apiEndpoint = "hannahfantasia.com/critterworld/critters/000.00_Mason/000.00_Mason.json";
+document.addEventListener("DOMContentLoaded", (event) => {
+  displayCritters()
+});
+
 const display = document.querySelector("#display-data");
-const input = document.querySelector("#input");
 
-
-function check(str){
-  if(str.match(/(\w*)\.json$/) == null){
-      console.log('false');
-      return false;
-  }
-  else {
-      console.log('true');
-      return true;
-  }
+const fetchSingle = async (url) => {
+  const strdata = fetch(url)
+  const result = (await strdata).json()
+  return result
 }
 
+const fetchCritters = async () => {
+  const base_url = "https://raw.githubusercontent.com/HannahFantasia/HannahFantasia-Site/main/critterworld/critters/"
+  const name_list = [
+    "000.00_Mason",
+    "001.00_Jube"]
 
-// FUNCTION TO FETCH A JSON FILE  
-const getData = async () => {
-  const res = await fetch(entries);
-  if (!res.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  const data = await res.json();
-  console.log(data)
-  return data;
+  let critter_objects = []
+  name_list.forEach((name) => {
+    const cur_url = `${base_url}${name}/${name}.json`
+
+    fetchSingle(cur_url).then((critter) => {
+      critter_objects.push(critter)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  })
+
+  return critter_objects
 }
 
-// FUNCTION TO VISUALISE THE DATA
-const displayUsers = async () => {
-  let query = input.value;
-
-  const payload = await getData();
-
-  let dataDisplay = payload.map((object) => {
-    const {name, species, age, occupation, personality, description} = object; // LIST YOUR JSON DATA HERE
-
+const displayCritters = async () => {
+  const critters = await fetchCritters();
+  
+  let dataDisplay = critters.map((critter) => {
+    const {Name, Age, Attributes} = critter; // LIST YOUR JSON DATA HERE
+    const {Species, Occupation, Personality, Description} = Attributes;
+    
     return `
     <div class="container">
-    <p>Name: ${name}</p>
-    <p>Species: ${species}</p>
-    <p>Age: ${age}</p>
-    <p>Occupation: ${occupation}</p>
-    <p>Personality: ${personality}</p>
-    <p>Description: ${description}</p>
+    <p>Name: ${Name}</p>
+    <p>Species: ${Species}</p>
+    <p>Age: ${Age}</p>
+    <p>Occupation: ${Occupation}</p>
+    <p>Personality: ${Personality}</p>
+    <p>Description: ${Description}</p>
     </div>
     `/////AND HERE ABOVE DIV^////
   }).join("");
-
+  
   display.innerHTML = dataDisplay;
 }
-
-displayUsers();
--- critterworld/test.js
+++ critterworld/test.js
@@ -1,36 +1,32 @@
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
-const dir = 'I:\\My Drive\\Website\\critterworld\\critters';
+let critter_objects = []
+const base_url = "https://raw.githubusercontent.com/HannahFantasia/HannahFantasia-Site/main/critterworld/critters/"
+const name_list = [
+      "000.00_Mason",
+      "001.00_Jube"
+    ]
 
-const traverse = function(dir, result = []) {
-    
-    // list files in directory and loop through
-    fs.readdirSync(dir).forEach((file) => {
-        
-        // builds full path of file
-        const fPath = path.resolve(dir, file);
-        
-        // prepare stats obj
-        const fileStats = {path: fPath };
+const fetchSingle = async (url) => {
+    const strdata = fetch(url)
+    const result = (await strdata).json()
+    return result
+  }
+  
+  const fetchCritters = async () => {
 
-        const targetExtension = ".js";
-
-        // is the file a directory ? 
-        // if yes, traverse it also, if no just add it to the result
-        if (fs.statSync(fPath).isDirectory()) {
-
-            fileStats.type = 'dir';
-            fileStats.files = [];
-            result.push(fileStats);
-            return traverse(fPath, fileStats.files)
-        }
-
-        result.push(fileStats);
-
-
-    });
-    return result;
-};
-
-console.log(util.inspect(traverse(dir), false, null));
+    name_list.forEach((name)=> {
+      const cur_url = `${base_url}${name}/${name}.json`
+  
+      fetchSingle(cur_url).then((critter) => {
+        critter_objects.push(critter)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  
+    })
+  
+    return critter_objects
+  }
+  
+  fetchCritters()
+  console.log(critter_objects);
